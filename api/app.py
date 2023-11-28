f19f967791af04a14aebc77ff29681ecce99b62b
Jerry Liu <953639086@qq.com>
2023-11-28 11:19:02 +0800

    theme

--- api/app.py
+++ api/app.py
@@ -4,20 +4,22 @@ from flask_bootstrap import Bootstrap5
 app = Flask(__name__)
 bootstrap = Bootstrap5(app)
 app.secret_key = 'your_secret_key'  # 设置一个安全密钥
+app.config['BOOTSTRAP_BOOTSWATCH_THEME'] = 'default'
+
 
 @app.route('/')
 def index():
-    print("***********************")
     return render_template('layout.html')
 
 @app.route('/abc')
 def bootswatch():
-    print("1234567890")
+    theme_list=['default', 'cerulean', 'cosmo', 'cyborg', 'darkly', 'flatly', 'journal', 'litera', 'lumen', 'lux', 'materia', 'minty', 'morph', 'pulse', 'quartz', 'sandstone', 'simplex', 'sketchy', 'slate', 'solar', 'spacelab', 'superhero', 'united', 'vapor', 'yeti', 'zephyr']
+    theme_now=app.config['BOOTSTRAP_BOOTSWATCH_THEME']
+    try:
+        app.config['BOOTSTRAP_BOOTSWATCH_THEME']=theme_list[theme_list.index(theme_now)+1]
+    except IndexError:
+        app.config['BOOTSTRAP_BOOTSWATCH_THEME']='default'
     flash(app.config['BOOTSTRAP_BOOTSWATCH_THEME'])
-    if app.config['BOOTSTRAP_BOOTSWATCH_THEME'] == 'litera':
-        app.config['BOOTSTRAP_BOOTSWATCH_THEME'] = 'morph'
-    else:
-        app.config['BOOTSTRAP_BOOTSWATCH_THEME'] = 'litera'
     return render_template('layout.html')
 
 if __name__ == '__main__':

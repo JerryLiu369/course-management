c965cf935afbac2bee0310c42675913228b9eccb
Jerry Liu <953639086@qq.com>
2023-12-17 19:37:50 +0800

    change host

--- api/app.py
+++ api/app.py
@@ -166,4 +166,4 @@ def process_selection():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True,port="0.0.0.0")
+    app.run(debug=True,host="0.0.0.0")
